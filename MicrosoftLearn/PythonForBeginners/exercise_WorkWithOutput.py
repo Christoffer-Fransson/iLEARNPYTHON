89621beaf1e4261958da43c3ec0cb03009746b15
Christoffer Fransson <christoffer.fransson2@yh.nackademin.se>
2023-07-28 12:52:31 +0200

    First excersise complete, Variables, operators, string conversion and print

---
+++ MicrosoftLearn/PythonForBeginners/exercise_WorkWithOutput.py
@@ -0,0 +1,10 @@
+# Display today's date
+# Add the code below
+
+from datetime import date
+print(date.today())
+
+# Build a unit converter
+parsecs = 11
+lightyears = parsecs * 3.26
+print((str(parsecs)) + " parsecs is " + str(lightyears) + " lightyears") # Outputs Nr of parsecs and how many lightyears they are.
