27f5dc333eac2195b3f0023ec5eeb0855d213137
Christoffer Fransson <christoffer.fransson2@yh.nackademin.se>
2023-07-28 13:14:58 +0200

    Created file to have overview of Python modules

---
+++ python_modules.py
@@ -0,0 +1,9 @@
+# This file contain a overview list of python modules, in alphabetical order
+
+# sys.argv - enables you to retrieve command-line arguments. Is an array or data structure. First position = 0 and is the program name
+            # second position = 1 contains the first argument.
+# Code example
+import sys
+print(sys.argv)
+print(sys.argv[0]) # program name
+print(sys.argv[1]) # first arg
