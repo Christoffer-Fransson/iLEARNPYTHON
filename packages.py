49fa39b172e664770e48aa0eefa02bb2e6cf1619
Christoffer Fransson <christoffer.fransson2@yh.nackademin.se>
2023-07-31 09:43:28 +0200

    Added file as informationholder for package related info

---
+++ packages.py
@@ -0,0 +1,10 @@
+# this file contain infromation about python packages, packages that I have used over time and their explanations
+
+# installation of packages are done using pip. pip use the Python Package Index (PyPi) -> https://pypi.org/
+
+# example:
+# Console: (do this from the /env directory)
+pip install python-dateutil
+
+pip freeze # - Prints a list of installed packages in the terminal (can be run inside the virtual environment to see what it has installed
+                # but also OUTSIDE of the virtual environment to see what libraries is installed on the computer)
