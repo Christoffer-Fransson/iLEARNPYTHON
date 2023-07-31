4fe20614cd9032b737a57a7f79f5ed4ce8a932b3
Christoffer Fransson <christoffer.fransson2@yh.nackademin.se>
2023-07-31 10:09:15 +0200

    added: virtual environment info

--- python_modules.py
+++ python_modules.py
@@ -7,3 +7,14 @@ import sys
 print(sys.argv)
 print(sys.argv[0]) # program name
 print(sys.argv[1]) # first arg
+
+
+#venv module - creates a vritual environment where your python program is run with all dependencies it needs without affecting other installed versions of python etc. 
+                # Code: "python -m venv Env" - is run as a terminal command
+                # ! never put your program files in the env directory. Rather put program files in a directory called src
+                
+                # to ACTIVATE a virtual environment:
+                    # Console: c:\...\env\Scripts\activate
+                    # Bash: source env/bin/activate    eg. go to bin folder and write "source activate" in bash temrinal. 
+                    # you know when it i activated by (env) path/to/project
+                # to step out of the virtual environment -> Console: deactivate
