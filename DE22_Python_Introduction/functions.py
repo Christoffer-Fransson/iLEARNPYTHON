3264b0effc963b71c15a5fd2a9846c0f3386bc55
Christoffer Fransson <christoffer.fransson2@yh.nackademin.se>
2023-09-03 15:53:43 +0200

    updated with minor info

---
+++ DE22_Python_Introduction/functions.py
@@ -0,0 +1,8 @@
+# Function = A block of reusable code
+#             place () after the functino name to invoke it
+
+def happy_birthday(name):
+   print (f"Happy birthday {name}!") 
+    
+happy_birthday("Bro")
+
