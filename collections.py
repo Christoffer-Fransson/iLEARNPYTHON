c97162b246e02f8ee5877133d9b4dd8736dc4653
Christoffer Fransson <christoffer.fransson2@yh.nackademin.se>
2023-09-03 16:02:04 +0200

    Create: collections.py to gather information about lists, sets and tuples

---
+++ collections.py
@@ -0,0 +1,10 @@
+# collection = single "variable" used to store multiple values
+#     List  = [] ordered and changeable. Duplicated OK
+#     set   = {} unordered and ummutable, but Add/Remove OK. NO duplicates
+#     Tuple = () ordered and unchangeable. Duplicates OK. FASTER than List.
+
+# https://www.youtube.com/watch?v=gOMW_n2-2Mw               15:05 min
+
+
+
+
